8bcaaa57566b74007a1e321a17104fc29e75d50f
William Pescherine <wkpescherine@gmail.com>
2019-05-28 03:55:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,6 @@
 def first_challenge
    epic_tragedy = {
+     epic_tragedy = {
   :montague => {
     :patriarch => {  
       :name => "Lord Montague",
@@ -54,4 +55,5 @@ def first_challenge
     ]
   }
 }
+   }
 end
