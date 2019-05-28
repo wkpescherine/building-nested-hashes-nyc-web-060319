a9a12947b31f73ba3593b7b2b7208cc5a8d30e66
William Pescherine <wkpescherine@gmail.com>
2019-05-28 04:03:19 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,16 @@
 def first_challenge
    epic_tragedy = {
-    :montague => {},
-    :capulets => {}
+    :montague => {
+      :patriarch => {},
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => {}
+    },
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => {}
+    }
    }
 end
