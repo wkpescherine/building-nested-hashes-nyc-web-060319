ab90d2c626eb461b26e61c7a853ade00db67afaa
William Pescherine <wkpescherine@gmail.com>
2019-05-28 04:09:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,8 +4,8 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+      :heroine => {},
+      :heroine_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
